481c6310fc824871cb5fca71584893bb129401e7
Jimmy Yeung <worldofpuzzle@yahoo.com>
2021-11-01 21:20:09 -0700

    adding more comments

--- verify.php
+++ verify.php
@@ -5,7 +5,6 @@
     <!-- Bootstrap -->
     <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
     <meta content="utf-8" http-equiv="encoding">
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -24,11 +23,13 @@
         <br> Here were the submitted informations:
     </center>
     <br>
+    <!-- The verified page will have our input informations. -->
     <div class="container center-div">
         <div class="container jumbotron jumbotron-fluid" id="main-container">
             <div class="container center-div">
                 <div class="row">
                     <div class="col-md-5">
+                        <!-- The output will display personal information. -->
                         <div class="row"><b>Full Name:</b> <?php echo $_POST["firstName"]; ?> <?php echo $_POST["lastName"]; ?></div><br>
                         <div class="row"><b>Address:</b> <?php echo $_POST["street"]; ?>, <?php echo $_POST["city"]; ?>, <?php echo $_POST["state"]; ?> <?php echo $_POST["zip"]; ?></div><br>
                         <div class="row"><b>Title:</b> <?php echo $_POST["title"]; ?></div><br> 
@@ -37,6 +38,7 @@
                         <div class="row"><b>Phone:</b> <?php echo $_POST["phone"]; ?></div><br>
                         <div class="row"><b>E-mail:</b> <?php echo $_POST["email"]; ?></div><br>
                     </div>
+                    <!-- The map displays the right side of user's input values.-->
                     <div id="map"></div> 
                 </div>
                 
@@ -49,13 +51,13 @@
             //source: https://developers.google.com/maps/documentation/javascript/examples/geocoding-simple
             //source: https://github.com/zipingH/csc642-forms/blob/master/csc642_forms/results.php
             // He has free copyright license, so I decide to reference his codes to. 
-            //function to generate and display google map
+            // The whole script here displays map on my verified page.
             function initMap() {
                 var myLatLng = {
                     lat: 37.77493,
                     lng: -82.41942
                 };
-
+                // myLatlng decides the zoom of the map. 
                 var map = new google.maps.Map(document.getElementById('map'), {
                     zoom: 15,
                     center: new google.maps.LatLng(44.5403, -78.5463),
@@ -65,7 +67,8 @@
                 
                 geocodeAddress(geocoder, map);
 
-
+                // marker determines the postion of the map.
+                // The position of the map will be center on the location of us living there. 
                 var marker = new google.maps.Marker({
                     position: myLatLng,
                     map: map,
@@ -74,8 +77,9 @@
 
             }
             
-            //get the geocode of the address
+            // The geocode needs our street address. 
             function geocodeAddress(geocoder, resultsMap) {
+                // Creates address based on our living location.
                 var address = "<?php echo $_POST["street"]; ?>" + ", " + "<?php echo $_POST["city"]; ?>" + ", " + "<?php echo $_POST["state"]; ?>" + ", " + "<?php echo $_POST["zip"]; ?>";
                 geocoder.geocode({
                     'address': address
@@ -93,7 +97,7 @@
             }
             google.maps.event.addDomListener(window, 'load', initialize);
         </script>
-        <!-- I am using  -->
+        <!-- I am using google map key to show the google map on screen.-->
         <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCltIFdR4Oi-XcoM9p3BMcqdLBN1mAbMcU&callback=initMap">
         </script>
 </body>
